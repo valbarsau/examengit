4f7b9158b868909b6ca5b6c901f3edcec2462bd2
valbarsau <valentinbarquero@gmail.com>
2015-11-19 13:43:08 +0100

    Modificado nombre perimetro.py en next
    Renombrada la funcion perimeter a perimetro y modificada para que devuelva 0 en next

--- pentagono.py
+++ pentagono.py
@@ -2,8 +2,8 @@ class pentagono:
 	def __init__(self, lado):
 		self.lado = lado
 
-        def perimeter(self):
-   	        return 5 + self.lado
+        def perimetro(self):
+   	        return 0
 
 	def area(self):
 		return 0
